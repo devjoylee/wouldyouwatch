df0375672771c1cb27f88509e1c72dcaa9c63f75
Joy Lee <joylee.dev@gmail.com>
2023-03-18 22:46:10 -0700

    feat : AppLoading component

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import React, { useState } from 'react';
+import { Text } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    // preloading APIs, assets or database..
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    // await Font.loadAsync(Ionicons.load);
+  };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!ready) {
+    return <AppLoading onFinish={onFinish} onError={console.error} startAsync={startLoading} />;
+  }
+
+  // loading done
+  return <Text>We are done loading</Text>;
+}
